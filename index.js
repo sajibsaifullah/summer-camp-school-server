7b1b53a9556e44b90a0b1d4efba0d81e3370801e
sajibsaifullah <dusajibphy@gmail.com>
2023-06-07 10:54:23 +0600

    check server on test

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ app.use(express.json());
 app.get('/', (req, res)=>{
     res.send("Summer Camp School is running...")
 })
+app.get('/test', (req, res)=>{
+    res.send("Summer Camp School is running on test")
+})
 
 app.listen(port, ()=>{
     console.log(`Summer camp school is running on port: ${port}`)
