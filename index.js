153c9e2d50d0bac99e22c125971fe34a84b82538
sajibsaifullah <dusajibphy@gmail.com>
2023-06-10 19:57:02 +0600

    class api

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const jwt = require("jsonwebtoken");
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mzdz3ie.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -19,16 +18,108 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const usersCollection = client.db("summerSchoolDb").collection("users");
+    const classCollection = client.db("summerSchoolDb").collection("classes");
+
+    // jwt
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
+
+    // users api
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      // console.log(user)
+      const query = { email: user.email };
+      const exitingUser = await usersCollection.findOne(query);
+      // console.log('existing user ',exitingUser)
+      if (exitingUser) {
+        return res.send({});
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.get("/users/admin/:email", async (req, res) => {
+      const email = req.params.email;
+
+      //   if (req.decoded.email !== email) {
+      //     res.send({ admin: false });
+      //   }
+
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      const result = { admin: user?.role === "admin" };
+      res.send(result);
+    });
+
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    app.get("/users/instructor/:email", async (req, res) => {
+      const email = req.params.email;
+
+      //   if (req.decoded.email !== email) {
+      //     res.send({ instructor: false });
+      //   }
+
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      const result = { instructor: user?.role === "instructor" };
+      res.send(result);
+    });
+
+    app.patch("/users/instructor/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "instructor",
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    // class api
+
+    app.post("/classes", async (req, res) => {
+      const input = req.body;
+      const result = await classCollection.insertOne(input);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -36,15 +127,13 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("Summer Camp School is running...");
+});
+app.get("/test", (req, res) => {
+  res.send("Summer Camp School is running on test");
+});
 
-
-app.get('/', (req, res)=>{
-    res.send("Summer Camp School is running...")
-})
-app.get('/test', (req, res)=>{
-    res.send("Summer Camp School is running on test")
-})
-
-app.listen(port, ()=>{
-    console.log(`Summer camp school is running on port: ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Summer camp school is running on port: ${port}`);
+});
