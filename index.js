148b8e39b5948731b7ab75684c82811e9c732211
sajibsaifullah <dusajibphy@gmail.com>
2023-06-11 18:56:24 +0600

    updated

--- index.js
+++ index.js
@@ -81,10 +81,6 @@ async function run() {
     app.get("/users/instructor/:email", async (req, res) => {
       const email = req.params.email;
 
-      //   if (req.decoded.email !== email) {
-      //     res.send({ instructor: false });
-      //   }
-
       const query = { email: email };
       const user = await usersCollection.findOne(query);
       const result = { instructor: user?.role === "instructor" };
